1fb5c97a6db902e1de84d27dea7224111356927b
hydRAnger <armyiljfe@gmail.com>
2022-01-11 15:28:11 +0900

    feat: g123-game:MobileDrawer => g123-ui:Modal

---
+++ src/components/Organisms/Modal/index.tsx
@@ -0,0 +1,86 @@
+import { classnames } from '@/tailwindcss-classnames';
+import React, { useEffect, useState } from 'react';
+
+type WrapperProps = {
+  isOpen: boolean;
+};
+
+const Wrapper: React.FC<WrapperProps> = ({ isOpen, children }) => {
+  const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (drawerOpen !== isOpen) {
+      setTimeout(() => {
+        setDrawerOpen(isOpen);
+      }, 1000);
+    }
+  }, [drawerOpen, isOpen]);
+
+  return (
+    <div
+      className={`translate-height ${classnames(
+        'transition-all',
+        'block',
+        'w-full',
+        {
+          'h-full': drawerOpen,
+          'h-0': !drawerOpen,
+        },
+        'duration-300',
+        'ease-out',
+      )}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+const Content: React.FC = ({ children }) => {
+  return (
+    <div
+      aria-hidden="true"
+      className={classnames(
+        'animate-slide-in-bottom',
+        'block',
+        'box-border',
+        'bg-white',
+        'fixed',
+        'left-0',
+        'right-0',
+        '-bottom-3',
+        'z-50',
+        'w-full',
+        'overflow-x-hidden',
+        'overflow-y-hidden',
+        'rounded-t-xl',
+        'rounded-b-none',
+      )}
+      style={{ height: '36rem' }}
+      onClick={(e: React.MouseEvent): void => {
+        e.preventDefault();
+        e.stopPropagation();
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+type Props = {
+  isOpen?: boolean;
+};
+
+// Akira refs: https://gist.github.com/raymondsiu/b565b629d84b7085d073934eee4da6ab
+const Modal: React.FC<Props> = ({ isOpen = false, children }) => {
+  return (
+    <Wrapper isOpen={isOpen}>
+      <Content>{children}</Content>
+    </Wrapper>
+  );
+};
+
+Modal.defaultProps = {
+  isOpen: false,
+};
+
+export default Modal;
--- src/components/Organisms/index.ts
+++ src/components/Organisms/index.ts
@@ -1,1 +1,2 @@
 export { default as Dialog, DialogContainer, dialog } from './Dialog';
+export { default as Modal } from './Modal';

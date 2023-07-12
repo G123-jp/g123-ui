1efd28ccac83d088d13d019ddcab83429f9e50d0
hydRAnger <armyiljfe@gmail.com>
2023-07-12 16:37:43 +0900

    feat: update Summary.stories

--- src/components/Summary.stories.tsx
+++ src/components/Summary.stories.tsx
@@ -9,11 +9,30 @@ import {
   CloseButton,
   Logo,
   Switch,
+  Icon,
 } from '@/components/Atoms';
 import { Carousel } from '@/components/Molecules';
 import { StoryFn, Meta } from '@storybook/react';
 import React from 'react';
 
+import {
+  NotificationOutlined,
+  HomeOutlined,
+  GameOutlined,
+  ChatOutlined,
+  EarchOutlined,
+  ProfileOutlined,
+  NotificationFilled,
+  HomeFilled,
+  GameFilled,
+  ChatFilled,
+  EarchFilled,
+  ProfileFilled,
+  ChatTwoTone,
+  EarchTwoTone,
+} from './Atoms/Icon';
+
+const { ProfileTwoTone, NotificationTwoTone, HomeTwoTone, GameTwoTone } = Icon;
 export default {
   title: 'Summary',
 } as Meta;
@@ -113,6 +132,36 @@ const Template: StoryFn = () => (
           console.info(`G123 Switch ${checked}`);
         }}
       />
+    </div>
+
+    {/* Icons */}
+    <div className="flex flex-col gap-2">
+      <div className="flex gap-4 text-neutral-800">
+        <NotificationOutlined />
+        <HomeOutlined className="text-blue-500" />
+        <GameOutlined className="text-highlight" style={{ color: 'orange' }} />
+        <ChatOutlined />
+        <EarchOutlined />
+        <ProfileOutlined />
+      </div>
+
+      <div className="flex gap-4 text-neutral-800">
+        <NotificationFilled />
+        <HomeFilled className="text-blue-500" />
+        <GameFilled className="text-highlight" style={{ color: 'orange' }} />
+        <ChatFilled />
+        <EarchFilled />
+        <ProfileFilled />
+      </div>
+
+      <div className="flex gap-4 text-neutral-800">
+        <NotificationTwoTone />
+        <HomeTwoTone className="text-blue-500" />
+        <GameTwoTone className="text-highlight" style={{ color: 'orange' }} />
+        <ChatTwoTone />
+        <EarchTwoTone />
+        <ProfileTwoTone />
+      </div>
     </div>
   </div>
 );

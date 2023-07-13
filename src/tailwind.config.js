e322ea728ac5ab7eb49d425ac64396b214b2abfe
hydRAnger <armyiljfe@gmail.com>
2023-07-13 18:29:42 +0900

    feat: refresh colors

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -49,10 +49,51 @@ module.exports = {
       colors: {
         transparent: 'transparent',
         current: 'currentColor',
+
+        // Akira: 1.x palette, maybe DEPRECATED in the future
         primary: '#136c72',
         secondary: '#d9ede2',
         highlight: '#e3ff34',
         danger: '#ff385c',
+
+        // Akira: 2.x palette, Backward Compatibility
+        // Brand
+        'brand-primary-base': '#e3ff34',
+        'brand-primary-secondary': '#d2ef1c',
+        'brand-primary-container': '#f1ff99',
+        'brand-primary-bg': '#fafede',
+        'brand-secondary-base': '#136c72',
+        'brand-secondary-secondary': '#89b5b8',
+        'brand-secondary-container': '#d9ede2',
+        'brand-secondary-bg': '#eef6ef',
+        // Font
+        'font-primary': '#262626',
+        'font-secondary': '#8c8c8c',
+        'font-disabled': '#d9d9d9',
+        'font-overlay': '#ffffff',
+        // Link
+        'link-default': '#136c72',
+        'link-disabled': '#89b5b8',
+        // Semantic - error
+        'error-default': '#f6375a',
+        'error-disabled': '#faaebb',
+        'error-bg': '#fef2f5',
+        // Semantic - info
+        'info-default': '#fad511',
+        'info-disabled': '#f9e681',
+        'info-bg': '#fefbe9',
+        // Semantic - success
+        'success-default': '#61c630',
+        'success-disabled': '#b7e2a1',
+        'success-bg': '#f3faf0',
+        // Surface
+        'surface-primary': '#ffffff',
+        'surface-secondary': '#fafafa',
+        'surface-tertiary': '#f5f5f5',
+        'surface-quaternary': '#f0f0f0',
+        // Line
+        'line-border': '#262626',
+        'line-divider': '#f0f0f0',
       },
       animation: {
         'fade-in-bottom': 'fade-in-bottom 0.4s ease-out',

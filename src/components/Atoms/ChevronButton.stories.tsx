274e5fa93af66f1fa36d9370e50ee058eef72371
hydRAnger <armyiljfe@gmail.com>
2023-07-21 14:46:59 +0900

    feat: comments for deprecated ChevronButton/CloseButton

--- src/components/Atoms/ChevronButton.stories.tsx
+++ src/components/Atoms/ChevronButton.stories.tsx
@@ -6,7 +6,7 @@ import ChevronButton from './ChevronButton';
 import { ChevronButtonType } from '.';
 
 export default {
-  title: 'Atoms/ChevronButton',
+  title: 'Atoms/ChevronButton(deprecated)',
   component: ChevronButton,
   argTypes: {
     type: {
@@ -27,19 +27,24 @@ const Template: StoryFn<typeof ChevronButton> = ({
   className,
   style,
 }) => (
-  <>
-    <ChevronButton type={ChevronButtonType.up} />
-    <ChevronButton className="text-primary" type={ChevronButtonType.right} />
-    <ChevronButton className="text-secondary" type={ChevronButtonType.down} />
-    <ChevronButton className="text-danger" type={ChevronButtonType.left} />
-    <ChevronButton
-      className={className ?? ''}
-      color={color ?? ''}
-      disabled={disabled ?? false}
-      style={style ?? {}}
-      type={type ?? ChevronButtonType.forward}
-    />
-  </>
+  <div className="flex flex-col gap-4">
+    <h2 className="rounded-md bg-gray-100 px-4 py-2 text-gray-400">
+      {`DEPRECATED: use <Button /> with icon <Chevron(Down/Up/Left/Right)Outlined /> instead`}
+    </h2>
+    <div className="flex gap-2">
+      <ChevronButton type={ChevronButtonType.up} />
+      <ChevronButton className="text-primary" type={ChevronButtonType.right} />
+      <ChevronButton className="text-secondary" type={ChevronButtonType.down} />
+      <ChevronButton className="text-danger" type={ChevronButtonType.left} />
+      <ChevronButton
+        className={className ?? ''}
+        color={color ?? ''}
+        disabled={disabled ?? false}
+        style={style ?? {}}
+        type={type ?? ChevronButtonType.forward}
+      />
+    </div>
+  </div>
 );
 
 export const Default = Template.bind({});

a2865224d55a64745008e88b876a736ae9892c6d
hydRAnger <armyiljfe@gmail.com>
2023-08-29 15:29:03 +0900

    feat: add Molecules/Toast

---
+++ src/components/Molecules/Toast.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import toast, {
+  Toast,
+  ToastBar,
+  ToastIcon,
+  ToastOptions,
+  ToastType,
+  Toaster,
+} from 'react-hot-toast';
+
+export default toast;
+export { Toast, ToastBar, ToastIcon, ToastOptions, ToastType, Toaster };

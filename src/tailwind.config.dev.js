bfe3047235f9c348d8b920e0eba1c8ddf180fac3
hydRAnger <armyiljfe@gmail.com>
2024-01-09 15:52:55 +0900

    feat: connect customized dark mode flag('g123-dark') with storybook-dark-mode

--- src/tailwind.config.dev.js
+++ src/tailwind.config.dev.js
@@ -1,7 +1,10 @@
 import tailwindConfig from './tailwind.config.js';
 
-// Modify the darkMode configuration
-tailwindConfig.darkMode = 'class'; // or 'media' or 'false'
+// NOTE(Akira): Customized dark mode flag
+// ref: https://tailwindcss.com/docs/dark-mode#customizing-the-class-name
+tailwindConfig.darkMode = ['class', '[data-mode="g123-dark"]'];
+// NOTE(Akira): cover components which stand for storybook
+tailwindConfig.content = [...tailwindConfig.content, './.storybook/**/*.tsx'];
 
 // Export the modified configuration
 export default tailwindConfig;
